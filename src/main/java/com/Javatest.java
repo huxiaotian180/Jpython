2766808b3ca79be08010e932c480c07a35bf0218
e <huxt@baixin.net.cn>
2019-07-01 14:17:05 +0800

    add python调用java代码

---
+++ src/main/java/com/Javatest.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * Created by DELL on 2019/6/27.
+ */
+public class Javatest {
+
+  public String run(String str) {
+    return str;
+  }
+
+  public String testParam(String user) {
+    return "Hi,welcome" + user;
+  }
+
+}
